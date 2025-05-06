fe0c9117a7733354d87d327ca0bb5b0d7ca1c8b8
Ventax <maulanakenang24@gmail.com>
2025-05-06 14:55:50 +0700

    update avatar dan hero foto

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,16 +10,17 @@ const Hero = () => {
   return (
     <section
     id='home'
-    className=''
+    className='pt-28 lg:pt-36'
     >
-        <div className="">
+        <div className="container items-center lg:grid
+         lg:grid-cols-2 lg:gap-20">
 
             <div>
 
-                <div className="">
-                    <figure className="">
+                <div className="flex items-center gap-3">
+                    <figure className="img-box w-9 rounded-lg">
                         <img
-                         src="/images/avatar-1.jpg"
+                         src="/images/maulana-avatar.png"
                          width={40}
                          height={40}
                          alt="Maulana Potrait"
@@ -27,9 +28,12 @@ const Hero = () => {
                          />
                     </figure>
 
-                        <div className="">
-                            <span className="">
-                                <span className=""></span>
+                        <div className="flex items-center gap-1.5 text-zinc-400 
+                        text-sm tracking-wide">
+                            <span className="relative w-2 h-2 
+                            rounded-full bg-emerald-400">
+                                <span className="absolute inset-0 w-2 h-2 
+                            rounded-full bg-emerald-400 animate-ping"></span>
                             </span>
 
                             Available for work
